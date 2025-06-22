6e89f595396120bba557fcefcff7dc8e6901a2c2
Doridori2 <lhy010620@duksung.ac.kr>
2025-06-22 21:30:51 +0900

    fix: convert DB_PORT to Number for MySQL connection

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const rbpool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT), 
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   waitForConnections: true,
