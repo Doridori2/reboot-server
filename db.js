cdb74f4ac0381ac6798a4d506f07873745667d15
Doridori2 <lhy010620@duksung.ac.kr>
2025-06-22 17:59:36 +0900

    🔧 Fix: added DB_PORT to mysql pool config

--- db.js
+++ db.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const rbpool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   waitForConnections: true,
